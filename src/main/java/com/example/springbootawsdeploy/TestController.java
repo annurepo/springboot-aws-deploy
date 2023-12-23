925e357d511e1cb434c3fdb850e914b19835fdf8
Annu Sharma <Annus@niit.com>
2023-12-23 23:51:37 +0530

    New Addition

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -16,4 +16,9 @@ public class TestController {
     public String getMessage() {
         return "Second message from AWS Ecs";
     }
+    
+    @GetMapping("/msg")
+    public String getMessage1() {
+        return "Third message from AWS Ecs";
+    }
 }
