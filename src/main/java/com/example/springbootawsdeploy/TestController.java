aa6da83dcc36d846b9079ea96f03cea0689cf7e2
rim <annus@stackroute.in>
2025-03-26 10:11:50 +0530

    Token Added

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -1,3 +1,4 @@
+
 package com.example.springbootawsdeploy;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,18 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/demo")
 public class TestController {
-    @GetMapping("/data")
+
+	@GetMapping("/data")
     public String getData() {
         return "First message from AWS updated";
     }
 
-    @GetMapping("/message")
-    public String getMessage() {
-        return "Second message from AWS updated";
-    }
-    
-    @GetMapping("/msgadd")
-    public String getMessage1() {
-        return "Third message from AWS updated";
-    }
+//    @GetMapping("/message")
+//    public String getMessage() {
+//        return "Second message from AWS updated";
+//    }
+//    
+//    @GetMapping("/msgadd")
+//    public String getMessage1() {
+//        return "Third message from AWS updated";
+//    }
 }
